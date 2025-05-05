e166f45b72cec1e5e20702745d38d55c973b0dcc
Gupta, Utkarsh <Utkarsh.Gupta@netapp.com>
2025-05-05 15:59:58 +0530

    added ./ in base url in vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/mfe-dummy-vite-app",
+  base: "./mfe-dummy-vite-app",
   plugins: [react()],
 })

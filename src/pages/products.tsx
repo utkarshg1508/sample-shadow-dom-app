b35a1a8aae28972c68bd5f8f4d6869678f76be8d
Gupta, Utkarsh <Utkarsh.Gupta@netapp.com>
2025-05-05 21:13:40 +0530

    Added Table in Products tab

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Card, ComponentLoader, Heading, Layout, Text } from '@netapp/bxp-design-system-react';
+import { Button, Card, ComponentLoader, Heading, Layout, Table, Text, useTable } from '@netapp/bxp-design-system-react';
 import productsData from "../assets/products.json";
 
 interface ProductProps {
@@ -32,23 +32,49 @@ const Product = React.memo(({ name, price, src }: ProductProps) => {
     );
 });
 
+const ProductsGrid = () => {
+    return <Layout.Container>
+        <Layout.Grid>
+            {productsData.map((product) => {
+                const { id, name, price, src } = product;
+
+                return <Layout.GridItem lg={4} key={id}>
+                    <Card hasHoverEffect={true} style={{ padding: '20px', margin: '10px' }}>
+                        <Product id={id} name={name} price={price} src={src} />
+                    </Card>
+                </Layout.GridItem>
+            })}
+        </Layout.Grid>
+    </Layout.Container>
+}
+
+const ProductsTable = () => {
+    const tableColumns = [{
+        header: "Name",
+        accessor: "prod_name",
+        id: "prod_name"
+    },
+    {
+        header: "Price",
+        accessor: "prod_price",
+        id: "prod_price"
+    }];
+    const tableRows = productsData.map((product) => { return { prod_name: product.name, prod_price: product.price } });
+    const { organizedRows, columns } = useTable({ columns: tableColumns, rows: tableRows });
+
+    return <Layout.Container>
+        <Table columns={columns} rows={organizedRows} fixedHeight={500} />
+    </Layout.Container>
+}
+
+
 const Products = () => {
+
     return (
         <Layout.Page>
             <Layout.Content>
-                <Layout.Container>
-                    <Layout.Grid>
-                        {productsData.map((product) => {
-                            const { id, name, price, src } = product;
-
-                            return <Layout.GridItem lg={4} key={id}>
-                                <Card hasHoverEffect={true} style={{ padding: '20px', margin: '10px' }}>
-                                    <Product id={id} name={name} price={price} src={src} />
-                                </Card>
-                            </Layout.GridItem>
-                        })}
-                    </Layout.Grid>
-                </Layout.Container>
+                <ProductsGrid />
+                <ProductsTable />
             </Layout.Content>
         </Layout.Page>
     );

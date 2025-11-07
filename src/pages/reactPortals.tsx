8884fb5f408d178c5f05d903c91649a11f128c9b
Akula, Teja <Teja.Akula@netapp.com>
2025-11-07 10:02:20 +0530

    Revert "select inside modal"
    This reverts commit 4074e3a602bec7667f2a70d6050fd98ad79f2483.

--- src/pages/reactPortals.tsx
+++ src/pages/reactPortals.tsx
@@ -17,28 +17,11 @@ const TooltipComp = () => {
 
 const ModalComp = () => {
     const [open, setOpen] = useState(false);
-    const form = useForm({ name: "" });
-    const options = [
-        { label: "Option 1", value: 1 },
-        { label: "Option 2", value: 2 },
-        { label: "Option 3", value: 3 },
-        { label: "Option 4", value: 4 },
-        { label: "Option 5", value: 5 },
-        { label: "Option 6", value: 6 },
-        { label: "Option 7", value: 7 }
-    ];
     if (open) {
         return (
             <Modal>
                 <ModalHeader>Modal header</ModalHeader>
-                <ModalContent style={{ overflow: "hidden" }}>
-                    <FormFieldSelect label="Account"
-                        name="name"
-                        form={form}
-                        options={options}
-                        menuPortalTarget={document.body}
-                    />
-                </ModalContent>
+                <ModalContent>I am a modal with one button in the footer</ModalContent>
                 <ModalFooter>
                     <Button onClick={() => setOpen(false)}>Cancel</Button>
                 </ModalFooter>
